d41d01233b5acd348e000c675c2dfc61dce6fc46
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2023-08-21 17:42:42 -0400

    better buttons

--- src/components/quiz/AnswerButton.tsx
+++ src/components/quiz/AnswerButton.tsx
@@ -1,18 +1,39 @@
 interface OptionButtonProps {
   option: string;
   handleAnswer: (option: string) => void;
+  isCorrectAnswer: boolean;
+  isAnswered: boolean;
+  isSelected: boolean;
 }
 
 const AnswerButton: React.FC<OptionButtonProps> = ({
   option,
   handleAnswer,
+  isCorrectAnswer,
+  isAnswered,
+  isSelected,
 }) => {
+  let classNames =
+    "mb-2 block w-full rounded-md border py-2 transition-all duration-300 hover:scale-105";
+
+  if (isAnswered && isCorrectAnswer) {
+    classNames += " border-green-500";
+  } else {
+    classNames += " border-purple-600";
+  }
+
+  if (isSelected && !isCorrectAnswer && isAnswered) {
+    classNames += " bg-red-600";
+  }
+
+  if (isAnswered) {
+    classNames += " pointer-events-none";
+  } else {
+    classNames += " hover:border-purple-500 hover:bg-purple-600/25";
+  }
+
   return (
-    <button
-      className="mb-2 block w-full rounded-md border border-purple-600 py-2 transition-all duration-300
-      hover:scale-105 hover:border-purple-500 hover:bg-purple-600/25 "
-      onClick={() => handleAnswer(option)}
-    >
+    <button className={classNames} onClick={() => handleAnswer(option)}>
       <span className="text-lg font-medium text-white">{option}</span>
     </button>
   );
