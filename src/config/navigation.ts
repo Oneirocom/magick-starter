48a472187f81a8eba1c514440d8b7774e093eae9
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2023-09-06 18:32:57 -0400

    remove battle for now

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -1,7 +1,6 @@
 export const navigation = [
   { name: "Home", href: "/" },
   { name: "Quiz", href: "/quiz" },
-  { name: "Battle", href: "/battle" },
   { name: "Image Generation", href: "/image" },
 ];
 
